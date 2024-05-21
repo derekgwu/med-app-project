a0ee963a3d6c672fda485b0678ed6865e5ef1e5b
Derek Chen <dchen36@gwmail.gwu.edu>
2024-05-21 11:39:16 -0400

    added linking and images

--- open_guidelines/open_guidelines_api.py
+++ open_guidelines/open_guidelines_api.py
@@ -5,12 +5,12 @@ def get_webpage_source(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     html_source = soup.prettify()
-    with open('./assets/new.html', 'w') as file:
+    with open('./assets/triage.html', 'w') as file:
         file.write(html_source)
     return html_source
    
 
 # Example usage
-url = 'https://www.openguidelines.net/data/set_1011/html/C3_ResuscNewborn.html'
+url = 'https://www.openguidelines.net/data/set_1011/html/C1_Triage.html'
 source_code = get_webpage_source(url)
 print(source_code)
